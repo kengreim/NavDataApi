aa12deb6d7371bdcdce72e542b89a3527d2433fa
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-04-20 12:03:06 -0700

    Delete old code

--- Program.cs
+++ Program.cs
@@ -25,27 +25,3 @@ app.UseFastEndpoints();
 app.UseCors(corsBuilder => corsBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.Services.UseScheduler(scheduler => { scheduler.Schedule<CifpUpdateService>().Hourly().RunOnceAtStart(); });
 app.Run();
-
-// var lines = File.ReadLines("CIFP/FAACIFP18");
-// var meta = Meta424.Create(Supplement.V18);
-// var data = Data424.Create(meta, lines, out var skipped, out var invalid);
-//
-//
-// var arrivalService = new ArrivalService(data);
-// var combinedArrivals = arrivalService.GetCombinedArrivals("KSFO");
-//
-// foreach (var arrival in combinedArrivals)
-// {
-//     Console.WriteLine($"Arrival: {arrival.ArrivalIdentifier}");
-//     foreach (var sequence in arrival.Sequences)
-//     {
-//         Console.WriteLine($"  Transition: {sequence.Transition}");
-//         Console.WriteLine("  Points:");
-//         foreach (var point in sequence.Points)
-//         {
-//             Console.WriteLine($"    - {point}");
-//         }
-//     }
-//
-//     Console.WriteLine();
-// }
